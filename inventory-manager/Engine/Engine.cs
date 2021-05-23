dc1cadd6a8e85af30061fa92facfdcc086af2ce2
Marc_Alx <marc-alx@outlook.com>
2021-05-23 16:13:09 +0200

    [doc] Add comments and clean unsused methods

--- inventory-manager/Engine/Engine.cs
+++ inventory-manager/Engine/Engine.cs
@@ -7,61 +7,134 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Engine
 {
+    /// <summary>
+    /// Interface for moving things
+    /// </summary>
     public interface IMovable
     {
+        /// <summary>
+        /// Move up
+        /// </summary>
         void MoveUp();
 
+        /// <summary>
+        /// Move down
+        /// </summary>
         void MoveDown();
 
+        /// <summary>
+        /// Move left
+        /// </summary>
         void MoveLeft();
 
+        /// <summary>
+        /// Move right
+        /// </summary>
         void MoveRight();
     }
 
+    /// <summary>
+    /// Interface to rotate thing
+    /// </summary>
     public interface IRotatable
     {
+        /// <summary>
+        /// Current rotation angle, in radians
+        /// </summary>
+        float RotationAngle { get; }
+
+        /// <summary>
+        /// Rotate clockwise
+        /// </summary>
         void RotateClockwise();
 
+        /// <summary>
+        /// Rotate anti clockwise
+        /// </summary>
         void RotateAntiClockwise();
     }
 
+    /// <summary>
+    /// Interface for drawing things
+    /// </summary>
     public interface IDrawable
     {
+        /// <summary>
+        /// Texture to draw
+        /// </summary>
         Texture2D Texture2D { get; }
 
+        /// <summary>
+        /// Scale for texture
+        /// </summary>
         Vector2 Scale => new Vector2(
             this.DrawingWidth / this.Texture2D.Width,
             this.DrawingHeight / this.Texture2D.Height);
 
-        float Angle { get; }
+        /// <summary>
+        /// Rotation angle
+        /// </summary>
+        float RotationAngle { get; }
 
+        /// <summary>
+        /// Final drawing size
+        /// </summary>
         int DrawingWidth { get; }
 
+        /// <summary>
+        /// Final drawing height
+        /// </summary>
         int DrawingHeight { get; }
 
+        /// <summary>
+        /// Drawing position
+        /// </summary>
         Vector2 Position { get; }
 
+        /// <summary>
+        /// Update drawing according to size
+        /// </summary>
+        /// <param name="device"></param>
+        /// <param name="drawingWidth"></param>
+        /// <param name="drawingHeight"></param>
         void Update(GraphicsDevice device, int drawingWidth, int drawingHeight);
     }
 
+    /// <summary>
+    /// Represents an itme in a grid
+    /// </summary>
     public interface IGridItem
     {
+        /// <summary>
+        /// Position of the item
+        /// </summary>
         Vector2 Position { get; }
 
+        /// <summary>
+        /// Grid in which the item is placed
+        /// </summary>
         IGrid ReferenceGrid { get; }
-
-        bool FitsIn(IGrid grid);
     }
 
+    /// <summary>
+    /// Represents a grid
+    /// </summary>
     public interface IGrid
     {
+        /// <summary>
+        /// Width of the grid
+        /// </summary>
         int Width { get; }
 
+        /// <summary>
+        /// Height of the grid
+        /// </summary>
         int Height { get; }
-
-        bool CanHold(IGridItem item);
     }
 
+    /// <summary>
+    /// Represents an item that can move in a grid
+    /// </summary>
     public abstract class MovableGridItem : IGridItem, IGrid, IMovable
     {
         public Vector2 Position { get; protected set; }
@@ -71,16 +144,6 @@ namespace Engine
         public int Width { get; protected set; }
 
         public int Height { get; protected set; }
-
-       public bool CanHold(IGridItem item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool FitsIn(IGrid grid)
-        {
-            throw new NotImplementedException();
-        }
 
         public void MoveDown()
         {
@@ -123,14 +186,19 @@ namespace Engine
         }
     }
 
+    /// <summary>
+    /// Represents an item that can move an rotate in a grid
+    /// </summary>
     public abstract class MovableRotatableGridItem : MovableGridItem, IRotatable
     {
+        public float RotationAngle { get; private set; } = 0;
+
         public void RotateAntiClockwise()
         {
             var w = this.Width;
             this.Width = this.Height;
             this.Height = w;
-            //this.Angle -= Convert.ToSingle(Toolkit.ConvertToRadians(90));
+            this.RotationAngle -= Convert.ToSingle(Toolkit.ConvertToRadians(90));
         }
 
         public void RotateClockwise()
@@ -138,21 +206,24 @@ namespace Engine
             var w = this.Width;
             this.Width = this.Height;
             this.Height = w;
-            //this.Angle += Convert.ToSingle(Toolkit.ConvertToRadians(90));
+            this.RotationAngle += Convert.ToSingle(Toolkit.ConvertToRadians(90));
         }
     }
 
+    /// <summary>
+    /// Represents a pointer, basically a movable grid item of size 1x1
+    /// </summary>
     public class Pointer : MovableGridItem, IDrawable
     {
         public Texture2D Texture2D { get; protected set; }
 
-        public float Angle { get; protected set; }
+        public float RotationAngle { get; protected set; }
 
         public int DrawingWidth { get; protected set; }
 
         public int DrawingHeight { get; protected set; }
 
-        bool hasTexture = false;
+        private bool hasTexture = false;
 
         public Pointer(Texture2D texture2D, IGrid referenceGrid)
         {
@@ -170,11 +241,6 @@ namespace Engine
             }
         }
 
-        public new bool CanHold(IGridItem item)
-        {
-            return false;
-        }
-
         public void Update(GraphicsDevice device, int drawingWidth, int drawingHeight)
         {
             if (!hasTexture)
@@ -185,22 +251,40 @@ namespace Engine
         }
     }
 
+    /// <summary>
+    /// Represents an item (extends it to your needs) that can move and rotate
+    /// </summary>
     public abstract class Item : MovableRotatableGridItem, IDrawable
     {
         public Texture2D Texture2D { get; protected set; }
-
-        public float Angle { get; protected set; }
 
         public int DrawingWidth { get; protected set; }
 
         public int DrawingHeight { get; protected set; }
 
+        public float Angle
+        {
+            get
+            {
+                return this.RotationAngle;
+            }
+        }
+
+        private bool hasTexture = false;
+
         public void Update(GraphicsDevice device, int drawingWidth, int drawingHeight)
         {
-            throw new NotImplementedException();
+            if (!hasTexture)
+            {
+                this.Texture2D = new Texture2D(device, drawingWidth, drawingHeight, false, SurfaceFormat.Color);
+                this.Texture2D.SetData(Enumerable.Range(0, drawingWidth * drawingHeight).Select(i => Color.LightGreen).ToArray());
+            }
         }
     }
 
+    /// <summary>
+    /// The Inventory, a drawable grid, that forward Movable and Rotatable to Pointer or selected item
+    /// </summary>
     public class Inventory : IMovable, IRotatable, IGrid, IDrawable
     {
         private Pointer _pointer;
@@ -219,7 +303,7 @@ namespace Engine
             this.Width / this.Texture2D.Width,
             this.Height / this.Texture2D.Height);
 
-        public float Angle { get { return 0; } }
+        public float RotationAngle { get { return 0; } }
 
         public Vector2 Position { get; protected set; }
 
@@ -262,11 +346,6 @@ namespace Engine
 
             this.Texture2D = texture2D;
             this.hasTexture = this.Texture2D != null;
-        }
-
-        public bool CanHold(IGridItem item)
-        {
-            throw new NotImplementedException();
         }
 
         public void MoveDown()
@@ -342,7 +421,7 @@ namespace Engine
             if (!this.hasTexture)
             {
                 this.Texture2D = new Texture2D(device, drawingWidth, drawingHeight, false, SurfaceFormat.Color);
-                this.Texture2D.SetData(Enumerable.Range(0, drawingWidth * drawingHeight).Select(i => Color.Blue).ToArray());
+                this.Texture2D.SetData(Enumerable.Range(0, drawingWidth * drawingHeight).Select(i => Color.LightCoral).ToArray());
             }
             this._pointer.Update(device, this._gridCellSizeWidth, this._gridCellSizeHeight);
         }
